0ca953899efc6b812617f247d8b8a18ea1140b80
vkg_001 <vkg360.vikas@gmail.com>
2026-05-24 14:48:53 +0530

    besic setup done

---
+++ backend/src/main/java/com/example/ByteForge/ByteForgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ByteForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteForgeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ByteForge/ByteForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ByteForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
